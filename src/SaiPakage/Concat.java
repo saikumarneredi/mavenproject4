83b5ff7cd719f1bb1e9d2ff4918389f97af43b41
ADMIN <ADMIN@DESKTOP-DKDQVD8>
2022-06-23 18:39:02 +0530

    see the changes

--- src/SaiPakage/Concat.java
+++ src/SaiPakage/Concat.java
@@ -6,13 +6,13 @@ package SaiPakage;
 public class Concat {
 
 	public static void main(String[] args) {
-		String s1= "sai";
-		string s2 = "java learning"
+		String s1= "saineredi";
+		String s2 = "Ramesh ";
 		s1.concat("sai") ;
-		s1=s1.concat("Neredi");
+		s2=s2.concat("learning java");
 		System.out.println(s1);
 		
-		System.out.println(s1);
+		System.out.println(s2);
 		
 	}
 
