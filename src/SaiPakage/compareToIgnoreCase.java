e2b7250832e74cd954f8b134a9ec3b5e4bb2f221
Nirosh kumar <107617199+NiroshKumarP@users.noreply.github.com>
2022-06-24 01:21:36 +0530

    Update compareToIgnoreCase.java

--- src/SaiPakage/compareToIgnoreCase.java
+++ src/SaiPakage/compareToIgnoreCase.java
@@ -5,7 +5,8 @@ public class compareToIgnoreCase {
 		String name="saiKumar";
 		String name2="saikumar";
 		String name3="saikumar";
-		String name4="neredi";
+		String name4="neredi"
+		String name6=	"learning"
 		System.out.println(name.compareToIgnoreCase(name2));
 		System.out.println(name2.compareToIgnoreCase (name3));
 		System.out.println(name3.compareToIgnoreCase (name4));
