5815b47e25e1c74dea52bdceb0e975be88fdadc2
Nirosh kumar <107617199+NiroshKumarP@users.noreply.github.com>
2022-06-23 23:46:12 +0530

    Update EqualsIgnoreCase.java

--- src/SaiPakage/EqualsIgnoreCase.java
+++ src/SaiPakage/EqualsIgnoreCase.java
@@ -7,12 +7,14 @@ public class EqualsIgnoreCase {
 		String name2="Saikumar";
 		String name3="saikumar";
 		String name4="neredi";
+		string name6="changes";
+		
 		System.out.println(name.equalsIgnoreCase(name2));
 		System.out.println(name2.equalsIgnoreCase(name3));
 		System.out.println(name3.equalsIgnoreCase(name4));
 	
 
-
+ 
 	}
 
 }
