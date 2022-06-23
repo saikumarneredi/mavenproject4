a9206b5838e8eaae92bfb3f6c259d159ee791419
saikumarneredi <107617072+saikumarneredi@users.noreply.github.com>
2022-06-23 02:07:33 -0700

    Update Concat.java

--- src/SaiPakage/Concat.java
+++ src/SaiPakage/Concat.java
@@ -6,7 +6,8 @@ package SaiPakage;
 public class Concat {
 
 	public static void main(String[] args) {
-		String s1= "sai";    
+		String s1= "sai";
+		string s2 = "java learning"
 		s1.concat("sai") ;
 		s1=s1.concat("Neredi");
 		System.out.println(s1);
